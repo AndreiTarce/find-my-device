307852871ed10b261ecf4833bac2c5268d95b0cd
Andrei Tarce <andrei.tarce@accesa.eu>
2022-12-03 21:36:44 +0200

    Added time to sensor data post requests.

--- functions/index.js
+++ functions/index.js
@@ -6,11 +6,17 @@ const { initializeApp } = require("firebase-admin");
 
 const app = express();
 admin.initializeApp();
+const firestore = admin.firestore();
 
 app.get("/", (req, res) => {});
 app.post("/", async (req, res) => {
     const sensor_data = req.body;
-    await admin.firestore().collection("date_senzor").add(sensor_data);
+    const dataToSend = {
+        time: admin.firestore.FieldValue.serverTimestamp(),
+        latitude: sensor_data.latitude,
+        longitude: sensor_data.longitude,
+    };
+    await admin.firestore().collection("date_senzor").add(dataToSend);
     res.status(201).send();
 });
 
