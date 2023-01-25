af67aa9ea19ec91c5cd129cba220c46cbd545e2e
AndreiTarce <tarceandrei@gmail.com>
2023-01-25 16:32:27 +0200

    Added illustration to history

--- src/pages/history/History.js
+++ src/pages/history/History.js
@@ -4,14 +4,19 @@ import { collection, query, onSnapshot } from "firebase/firestore";
 import { db } from "../../utils/firebase";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { addTripToHistory, sortTripsHistory, deleteTripFromHistory } from "../../actions";
+import {
+    addTripToHistory,
+    sortTripsHistory,
+    deleteTripFromHistory,
+} from "../../actions";
 import Trip from "../../components/TripsHistory/Trip";
 import TripsHistoryMap from "../../components/TripsHistory/TripsHistoryMap";
 import LoaderSpinner from "../../components/LoaderSpinner/LoaderSpinner";
-import { Container } from "react-bootstrap";
+import { Container, Image, Row } from "react-bootstrap";
 import DeleteTripModal from "../../components/TripsHistory/DeleteTripModal";
 import "./History.css";
 import { motion, AnimatePresence } from "framer-motion";
+import noTripsIllustration from "../../assets/no_trips.svg";
 
 const History = () => {
     const { user } = UserAuth();
@@ -33,7 +38,11 @@ const History = () => {
                         console.log(change.doc.data());
                         dispatch(sortTripsHistory());
                     } else if (change.type === "removed") {
-                        dispatch(deleteTripFromHistory(change.doc.data().startTime.toDate()));
+                        dispatch(
+                            deleteTripFromHistory(
+                                change.doc.data().startTime.toDate()
+                            )
+                        );
                     }
                 });
                 setLoading(false);
@@ -57,10 +66,16 @@ const History = () => {
                 <h1>Trip history</h1>
                 <div className="divider"></div>
             </Container>
-            {trips.length > 0 && trips.map((trip) => <Trip trip={trip} key={trip.startTime} />)}
+            {trips.length > 0 &&
+                trips.map((trip) => <Trip trip={trip} key={trip.startTime} />)}
             {trips.length <= 0 && (
-                <Container>
-                    <h2>You haven't recorded any trips yet.</h2>
+                <Container className="d-flex flex-column">
+                    <h2 className="pb-5">
+                        You haven't recorded any trips yet.
+                    </h2>
+                    <div className="illustration-container">
+                        <Image src={noTripsIllustration} id="illustration" />
+                    </div>
                 </Container>
             )}
         </>
