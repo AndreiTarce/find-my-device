79a1dd4845d2f17644f2a503ba6f8a1fd90b0ca3
Andrei Tarce <andrei.tarce@accesa.eu>
2023-02-03 14:06:35 +0200

    Added calculateRouteDistance cloud function

--- functions/index.js
+++ functions/index.js
@@ -9,10 +9,13 @@ admin.initializeApp();
 const firestore = admin.firestore();
 app.use(cors());
 
+/*HTTP FUNCTIONS*/
+
 app.get("/", (req, res) => {
     res.setHeader("Content-Type", "application/json");
     res.status(201).send({ response: 1 });
 });
+
 app.post("/", async (req, res) => {
     const sensor_data = req.body;
     const dataToSend = {
@@ -32,6 +35,26 @@ app.post("/delete_trips", async (req, res) => {
 });
 
 exports.senzor = functions.region("europe-west1").https.onRequest(app);
+
+/*Distance Calculator Function*/
+
+exports.calculateRouteDistance = functions
+    .region("europe-west1")
+    .firestore.document("users/{userId}/trips/{docId}")
+    .onCreate(async (snap, context) => {
+        // Get an object representing the document
+        // e.g. {'name': 'Marie', 'age': 66}
+        const newValue = snap.data();
+        // access a particular field as you would any JS property
+        const { startTime, endTime } = newValue;
+        const userId = context.params.userId;
+        const docId = context.params.docId;
+
+        await admin.firestore().doc(`users/${userId}/trips/${docId}`).update({ test: "test" });
+        // const name = newValue.name;
+
+        // perform desired operations ...
+    });
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
